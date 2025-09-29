164425d72f20998a30fd2cc9ddd5e965d319f424
PatrykWalenciak <157526938+PatrykWalenciak@users.noreply.github.com>
2025-09-29 23:00:41 +0200

    update

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const nav = document.getElementById("nav");
 burger.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+(function () {
+  emailjs.init("ZmHP0SEVfmZYCAHav"); // wstaw swój public key z EmailJS
+})();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".reservation-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_ccdd5jg", "template_83q8vfv", this).then(
+      () => {
+        alert("✅ Dziękujemy! Twoje zgłoszenie zostało wysłane.");
+        form.reset();
+      },
+      (error) => {
+        alert("❌ Wystąpił błąd, spróbuj ponownie.");
+        console.error("EmailJS error:", error);
+      }
+    );
+  });
+});
